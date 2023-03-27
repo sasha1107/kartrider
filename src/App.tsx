f1e6f0857a9b8a491e89201270e2163015e1ccf4
sasha1107 <sasha1107@naver.com>
2023-03-27 16:36:51 +0900

    initial setting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Home from './pages/Home'
+import { GlobalStyle } from './style/globalStyle'
+
+function App() {
+
+  return (
+    <div className="App">
+      <GlobalStyle/>
+      <Home />
+    </div>
+  )
+}
+
+export default App
