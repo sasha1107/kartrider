509903a4cef5b5ddcb0b0133807dd74a604493d2
Soohyun Jung <sasha1107@naver.com>
2023-03-27 22:28:24 +0900

    proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.nexon.co.kr/kart/v1.0',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true
+      }
+    }
+  }
 })
