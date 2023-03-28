f70155a137844ea4669edd5854bb744002441496
Soohyun Jung <sasha1107@naver.com>
2023-03-28 23:22:57 +0900

    ReactQueryDevtools 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import Match from './pages/Match'
 import styled from 'styled-components'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         </StyledContainer>
         <Footer/>
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
     </div>
   )
 }
