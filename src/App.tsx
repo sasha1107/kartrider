d151132d76b713c8e3c124fa8080e2d6e050cb8e
sasha1107 <sasha1107@naver.com>
2023-03-28 10:09:36 +0900

    Feat: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { GlobalStyle } from './style/globalStyle'
 import AddUser from './pages/AddUser'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Match from './pages/Match'
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
-      <Header/>
-      <Home />
-      <Match />
-      <Footer/>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/match' element={<Match />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   )
 }
