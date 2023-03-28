fd8f63dc0222399476ad3d735b551c7ade445b8e
sasha1107 <sasha1107@naver.com>
2023-03-28 11:42:38 +0900

    Style: 넥슨 폰트 설정

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -3,4 +3,15 @@ import reset from "styled-reset"
 
 export const GlobalStyle = createGlobalStyle`
     ${reset}
+
+    @font-face {
+    font-family: 'NEXON Lv1 Gothic OTF';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv1 Gothic OTF.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+    }
+
+    :root {
+        font-family : "NEXON Lv1 Gothic OTF"; 
+    }
 `
