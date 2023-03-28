c5e4bd10f27b66a774b5c2372daf24c8151cb56f
sasha1107 <sasha1107@naver.com>
2023-03-28 11:57:30 +0900

    Style: Footer 스타일 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Footer() {
     return (
-        <footer>
-            Data based on NEXON DEVELOPERS
-        </footer>
+        <StyledFooter>
+            Data based on <strong>NEXON DEVELOPERS</strong>
+        </StyledFooter>
     )
 }
+
+const StyledFooter = styled.footer`
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    text-align: center;
+    padding: 20px;
+    font-size: 12px;
+    background-color: #fff;
+    border-top: 1px solid #eee;
+    strong {
+        font-weight: 400;
+    }
+`
