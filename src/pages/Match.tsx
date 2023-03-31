aa47acb3a5f1ec2d23e4c50bf7eaad15cc9b08f6
sasha1107 <sasha1107@naver.com>
2023-03-31 17:06:03 +0900

    Feat: 매치 정보 페이지

--- src/pages/Match.tsx
+++ src/pages/Match.tsx
@@ -7,98 +7,123 @@ import { getName } from '../utils/getName'
 import { getParts } from '../utils/getParts'
 import styled from 'styled-components'
 import { parse } from '../components/Parse'
+import { Matches } from '../types/matches'
 
 export default function Match() {
-    const { isLoading ,isError, error, data } = useQuery(["findMatch"], fetchMatch)
-    const [index, setIndex] = useState(3);
-    const [length, setLength] = useState(0)
+    const { isLoading ,isError, error, data } = useQuery<Matches>(["findMatch"], fetchMatch)
+    const [index, setIndex] = useState(0);
     if (isLoading) return <Loading />  
     if (isError) return <Error />
-
-    console.log(data.matches[0])
-    console.log(index)
-    // console.log(data.matches[0].matches[0])
-    // console.log(data.matches[0].matches[0].player.pet)
+    // console.log()
+    const speedSolo = data.matches.filter(i => getName("gameType",i.matchType) === "스피드 개인전")[0].matches;
+    const speedTeam = data.matches[1];
     return (
         <div>
             <h1>{data.nickName}의 매치</h1>
             <p>
                 <span>{index + 1}</span>
                 /
-                <span>{length}</span>
+                <span>{speedSolo.length}</span>
             </p>
             <button
-            >
+            onClick={() => {
+                if (index !== 0) setIndex(index - 1)
+            }}>
                 이전매치
             </button>
             <button
-            >
+            onClick={() => {
+                if (index < speedSolo.length -1) setIndex(index + 1)
+            }}>
                 다음매치
             </button>
             <p>게임 타입</p>
-            <p>{getName("gameType", data.matches[0].matches[index].matchType)}</p>
+            {getName("gameType", speedSolo[index].matchType) ? <p>{getName("gameType", speedSolo[index].matchType)}</p> : <></>}
+
+            <p>트랙</p>
+            <StyledTrackImg src={`track/${speedSolo[index].trackId}.png`}/>
+            <p>{getName('track', speedSolo[index].trackId)}</p>
 
             <h2>캐릭터</h2>
+            {getName('character', speedSolo[index].character) ?
+            <>
             <p>캐릭터</p>
-            <img src={`character/${data.matches[0].matches[index].character}.png`}/>
-            <p>{getName('character', data.matches[0].matches[index].character)}</p>
+            <StyledCharacterImg src={`character/${speedSolo[index].character}.png`}/>
+            <p>{getName('character', speedSolo[index].character)}</p>
+            </>
+            :<></>}
 
-            {/* <p>펫</p> */}
-            {/* <p>{getName('pet', "87d9b446e122b6601b4a36590360c1f7b4dc263aaa2152c9ef75177373eaa409")}</p> */}
+            <p>펫</p>
+            {getName('pet', speedSolo[index].player.pet) ? 
+                <p>getName('pet', speedSolo[index].player.pet)</p>
+            : <>-</>
+            }
 
             <p>플라잉펫</p>
-            <p>{getName('flyingPet', data.matches[0].matches[index].player.flyingPet)}</p>
+            {getName('flyingPet', speedSolo[index].player.flyingPet) ?
+            <p>{getName('flyingPet', speedSolo[index].player.flyingPet)}</p>
+            :<>-</>}
 
             <h2>카트</h2>
+            {getName('kart', speedSolo[index].player.kart) ? 
+            <>
             <p>카트</p>
-            <img src={`kart/${data.matches[0].matches[index].player.kart}.png`}/>
-            <p>{getName('kart', data.matches[0].matches[index].player.kart)}</p>
+            <StyledCartImg src={`kart/${speedSolo[index].player.kart}.png`}/>
+            <p>{getName('kart', speedSolo[index].player.kart)}</p>
+            </>
+            :<>카트를 찾을 수 없습니다.</>}
 
             <h3>파츠</h3>
             <StyledPartsBox>
+                <p>엔진</p>
+                {speedSolo[index].player.partsEngine ? 
                 <div>
-                    <p>엔진</p>
                     <StyledPartsImgBox>
-                        <img src={getParts('partsEngine', data.matches[0].matches[index].player.partsEngine)?.ImageUrl}/>
-                        <p>{getParts('partsEngine', data.matches[0].matches[index].player.partsEngine)?.Name}</p>
+                        <img src={getParts('partsEngine', speedSolo[index].player.partsEngine)?.ImageUrl}/>
+                        <p>{getParts('partsEngine', speedSolo[index].player.partsEngine)?.Name}</p>
                     </StyledPartsImgBox>
-                    {parse(getParts('partsEngine', data.matches[0].matches[index].player.partsEngine)?.Effect)}
-                    <StyledPartsDesc>{getParts('partsEngine', data.matches[0].matches[index].player.partsEngine)?.Description}</StyledPartsDesc>
+                    {parse(getParts('partsEngine', speedSolo[index].player.partsEngine)?.Effect)}
+                    <StyledPartsDesc>{getParts('partsEngine', speedSolo[index].player.partsEngine)?.Description}</StyledPartsDesc>
                 </div>
+                : <>-</>}
+                
+                <p>핸들</p>
+                {speedSolo[index].player.partsHandle ? 
+                <div>
+                    <StyledPartsImgBox>
+                        <img src={getParts('partsHandle', speedSolo[index].player.partsHandle)?.ImageUrl}/>
+                        <p>{getParts('partsHandle', speedSolo[index].player.partsHandle)?.Name}</p>
+                    </StyledPartsImgBox>
+                    {parse(getParts('partsHandle', speedSolo[index].player.partsHandle)?.Effect)}
+                    <StyledPartsDesc>{getParts('partsHandle', speedSolo[index].player.partsHandle)?.Description}</StyledPartsDesc>
+                </div>
+                : <>-</>}
 
+                <p>킷</p>
+                {speedSolo[index].player.partsKit ? 
                 <div>
-                    <p>핸들</p>
                     <StyledPartsImgBox>
-                        <img src={getParts('partsHandle', data.matches[0].matches[index].player.partsHandle)?.ImageUrl}/>
-                        <p>{getParts('partsHandle', data.matches[0].matches[index].player.partsHandle)?.Name}</p>
+                        <img src={getParts('partsKit', speedSolo[index].player.partsKit)?.ImageUrl}/>
+                        <p>{getParts('partsKit', speedSolo[index].player.partsKit)?.Name}</p>
                     </StyledPartsImgBox>
-                    {parse(getParts('partsHandle', data.matches[0].matches[index].player.partsHandle)?.Effect)}
-                    <StyledPartsDesc>{getParts('partsHandle', data.matches[0].matches[index].player.partsHandle)?.Description}</StyledPartsDesc>
+                    {parse(getParts('partsKit', speedSolo[index].player.partsKit)?.Effect)}
+                    <StyledPartsDesc>{getParts('partsKit', speedSolo[index].player.partsKit)?.Description}</StyledPartsDesc>
                 </div>
+                :<>-</>}
 
+                <p>휠</p>
+                {speedSolo[index].player.partsWheel ?
                 <div>
-                    <p>킷</p>
                     <StyledPartsImgBox>
-                        <img src={getParts('partsKit', data.matches[0].matches[index].player.partsKit)?.ImageUrl}/>
-                        <p>{getParts('partsKit', data.matches[0].matches[index].player.partsKit)?.Name}</p>
+                        <img src={getParts('partsWheel', speedSolo[index].player.partsWheel)?.ImageUrl}/>
+                        <p>{getParts('partsWheel', speedSolo[index].player.partsWheel)?.Name}</p>
                     </StyledPartsImgBox>
-                    {parse(getParts('partsKit', data.matches[0].matches[index].player.partsKit)?.Effect)}
-                    <StyledPartsDesc>{getParts('partsKit', data.matches[0].matches[index].player.partsKit)?.Description}</StyledPartsDesc>
+                    {parse(getParts('partsWheel', speedSolo[index].player.partsWheel)?.Effect)}
+                    <StyledPartsDesc>{getParts('partsWheel', speedSolo[index].player.partsWheel)?.Description}</StyledPartsDesc>
                 </div>
-
-                <div>
-                    <p>휠</p>
-                    <StyledPartsImgBox>
-                        <img src={getParts('partsWheel', data.matches[0].matches[index].player.partsWheel)?.ImageUrl}/>
-                        <p>{getParts('partsWheel', data.matches[0].matches[index].player.partsWheel)?.Name}</p>
-                    </StyledPartsImgBox>
-                    {parse(getParts('partsWheel', data.matches[0].matches[index].player.partsWheel)?.Effect)}
-                    <StyledPartsDesc>{getParts('partsWheel', data.matches[0].matches[index].player.partsWheel)?.Description}</StyledPartsDesc>
-                </div>
+                : <>-</>}
             </StyledPartsBox>
-            <p>트랙</p>
-            <img src={`track/${data.matches[0].matches[index].trackId}.png`}/>
-            <p>{getName('track', data.matches[0].matches[index].trackId)}</p>
+            
         </div>
     )
 }
@@ -110,7 +135,7 @@ const StyledPartsBox = styled.div`
     gap: 10px;
     >div {
         display: grid;
-        grid-template-columns: .5fr 1fr 3fr 5fr;
+        grid-template-columns: 1fr 3fr 5fr;
         align-items: center;
         text-align: center;
         gap: 10px;
@@ -121,11 +146,24 @@ const StyledPartsImgBox = styled.div`
     flex-direction: column;
     align-items: center;
     font-size: 12px;
+    gap: 10px;
     img {
+        border: 1px solid #c1c1c1;
+        border-radius: 8px;
         width: 100px;
         height: 100px;
     }
 `
 const StyledPartsDesc = styled.p`
     font-size: 12px;
+`
+const StyledTrackImg = styled.img`
+    width: 150px;
+    height: 100px;
+`
+const StyledCharacterImg = styled.img`
+    width: 150px;
+`
+const StyledCartImg = styled.img`
+    width: 200px;
 `
